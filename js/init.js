2905b6f764d7a057e0a5a9002021f97aab976682
Sébastien Vaucher <seb.vaucher@gmail.com>
2015-05-24 02:46:33 +0200

    Contact

--- js/init.js
+++ js/init.js
@@ -5,6 +5,32 @@
 
     });
 })(jQuery);
+var recapOk = function() {
+    $('#submit').removeAttr('disabled');
+};
 $(document).ready(function(){
     $('.parallax').parallax();
+    $('#contact-form').validate({
+        errorClass: 'invalid',
+        validClass: 'valid',
+        errorElement: 'span',
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            subject: {
+                required: true,
+                maxlength: 140
+            },
+            message: {
+                required: true,
+                minlength: 20,
+                maxlength: 2000
+            }
+        }
+    });
 });
---
+++ message-sent.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: Message sent
+menu: none
+---
+
+Your message has been successfully sent, I'll get back to you as soon as possible.
