6f9e7a3fcc86c1e74ffe6afa8c5d261b672f8e33
Sébastien Vaucher <seb.vaucher@gmail.com>
2015-06-16 01:20:34 +0200

    Fix JS error

--- js/init.js
+++ js/init.js
@@ -10,27 +10,30 @@ var recapOk = function() {
 };
 $(document).ready(function(){
     $('.parallax').parallax();
-    $('#contact-form').validate({
-        errorClass: 'invalid',
-        validClass: 'valid',
-        errorElement: 'span',
-        rules: {
-            name: {
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            subject: {
-                required: true,
-                maxlength: 140
-            },
-            message: {
-                required: true,
-                minlength: 20,
-                maxlength: 2000
+    var contact = $('#contact-form');
+    if(contact.length) {
+        contact.validate({
+            errorClass: 'invalid',
+            validClass: 'valid',
+            errorElement: 'span',
+            rules: {
+                name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                subject: {
+                    required: true,
+                    maxlength: 140
+                },
+                message: {
+                    required: true,
+                    minlength: 20,
+                    maxlength: 2000
+                }
             }
-        }
-    });
+        });
+    }
 });
