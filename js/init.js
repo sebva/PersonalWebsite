ea936874e6270571f10fa310c7e8ff2df1ba0254
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-05-10 23:29:40 +0200

    XMPP chat using converse.js

--- js/init.js
+++ js/init.js
@@ -36,6 +36,21 @@ $(document).ready(function(){
                 }
             }
         });
+        require(['converse'], function (converse) {
+            converse.initialize({
+                authentication: 'anonymous',
+                jid: 'anon.vaucher.org',
+                auto_login: true,
+                bosh_service_url: 'https://s2.vaucher.org:5281/http-bind',
+                websocket_url: 'wss:s2.vaucher.org:5281/xmpp-websocket',
+                allow_chat_pending_contacts: true,
+                auto_subscribe: true,
+                auto_join_on_invite: true,
+                i18n: locales.en,
+                show_controlbox_by_default: false,
+                auto_join_rooms: ['web@conference.vaucher.org']
+            });
+        });
     }
 });
 $(window).on("load", function() {
