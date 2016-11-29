8e131e581275146d1fb8fe2df93c7934416fec41
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-11-29 15:51:44 +0100

    Add piwik

---
+++ js/stats.js
@@ -0,0 +1,9 @@
+var idSite = 1;
+var piwikTrackingApiUrl = 'https://stats.vaucher.org/piwik.php';
+
+var _paq = _paq || [];
+_paq.push(['setTrackerUrl', piwikTrackingApiUrl]);
+_paq.push(['setSiteId', idSite]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+
