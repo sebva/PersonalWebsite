066a480bb9c6465558bd4cd608a6608c628c512f
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-11-29 16:55:18 +0100

    Track events using piwik

--- js/stats.js
+++ js/stats.js
@@ -1,9 +1,19 @@
-var idSite = 1;
-var piwikTrackingApiUrl = 'https://stats.vaucher.org/piwik.php';
-
 var _paq = _paq || [];
-_paq.push(['setTrackerUrl', piwikTrackingApiUrl]);
-_paq.push(['setSiteId', idSite]);
+_paq.push(['setTrackerUrl', 'https://stats.vaucher.org/piwik.php']);
+_paq.push(['setSiteId', 1]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
+$(document).ready(function() {
+    $('#chat-button').on('click', function(e) {
+        _paq.push(['trackEvent', 'chat', 'start']);
+    });
+    $('.activator').on('click', function(e) {
+        var project = $(e.target).attr('data-project');
+        _paq.push(['trackEvent', 'project', 'open', project]);
+    });
+    $('.card-title').on('click', function(e) {
+        var project = $(e.target).attr('data-project');
+        _paq.push(['trackEvent', 'project', 'close', project]);
+    });
+});
 
