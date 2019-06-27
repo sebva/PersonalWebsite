3f828756549758f728aefc7eda42b041976005e9
Sébastien Vaucher <seb.vaucher@gmail.com>
2019-06-27 10:24:39 +0200

    Fix bug with links in collapsibles

--- js/init.js
+++ js/init.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var collapsibles = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(collapsibles, {});
 
-    document.querySelector('.collapsible').addEventListener('click', function(e) { e.preventDefault(); });
     document.querySelectorAll('a.activator').forEach(function(element) {
         element.addEventListener('click', function(e) { e.preventDefault(); });
     });
